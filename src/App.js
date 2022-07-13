fcd3ffbcf062731c8bc344c4f4035480b54fa078
Robert J <74118301+KermitTheSapo@users.noreply.github.com>
2022-07-13 08:36:39 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function Oi() {
+  return (
+    <div>
+      <h1>Prática React 03 - Component de function</h1>
+      <p>Robert José</p>
+    </div>
+  );
+}
